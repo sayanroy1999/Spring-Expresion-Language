2b5dab9c0f7ea663d28a7c6a355cca63e5a4cd72
Spring Demo <sroy030499@gmail.com>
2024-04-19 21:59:29 +0530

    commit2

--- src/main/java/com/example/demo13/Test.java
+++ src/main/java/com/example/demo13/Test.java
@@ -13,6 +13,53 @@ public class Test {
     @Value("#{2+4}")
     private int y;
 
+    //Passing static method through SpEL
+    @Value("#{T(java.lang.Math).sqrt(25)}")
+    private double z;
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                ", e=" + e +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    ////Passing static variable through SpEL
+    @Value("#{T(java.lang.Math).E}")
+    private double e;
+
+    //Passing objects through SpEL
+    @Value("#{ new java.lang.String('Tyson')}")
+    private String name;
+
+    public double getE() {
+        return e;
+    }
+
+    public void setE(double e) {
+        this.e = e;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
     public int getX() {
         return x;
     }
@@ -27,14 +74,6 @@ public class Test {
 
     public void setY(int y) {
         this.y = y;
-    }
-
-    @Override
-    public String toString() {
-        return "Test{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
     }
 
     public Test() {
